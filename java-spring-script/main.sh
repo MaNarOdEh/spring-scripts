78fc49d4478bdf7443bfdbed77a16c8bd9749c6f
Asal MacBook 2 <asal@asals-mbp.mynet>
2020-04-21 15:44:07 +0300

    create create_controller

--- java-spring-script/main.sh
+++ java-spring-script/main.sh
@@ -15,7 +15,7 @@ display_help(){
     echo "";
     echo "repository,r: --name,-n name of the repository ";
     echo "              --type,-t type of the repository [by default jbaRepository";
-    echo "              <T1,T2> type of first genric,second";
+    echo "              --T1,--T2 <T1,T2> type of first genric,second";
     echo "                [ by default <name,UUID] >";
     echo "";
     echo "controller,c:  --name,-n name of the controller";
@@ -78,6 +78,43 @@ create_entity(){
     ./script/create-model.sh $name $count "$@";
     exit 1;
 }
+create_repository(){
+    if [ -z "${1}" -o "$1" != "--name" -a "${1}" != "-n" ]
+    then
+       echo "Error! ";
+       display_help
+       
+    fi
+
+    if [ -z "$2" ]
+    then
+        echo "No passing argument";
+        display_help
+    fi
+    name=$2;
+    t=$4;
+    t_first=$6;
+    t_second=$7;
+    ./script/create-repository.sh $name $t $t_first $t_second;
+    exit 1;
+}
+create_controller(){
+    if [ -z "${1}" -o "$1" != "--name" -a "${1}" != "-n" ]
+    then
+       echo "Error! ";
+       display_help
+       
+    fi
+
+    if [ -z "$2" ]
+    then
+        echo "No passing argument";
+        display_help
+    fi
+    ./script/create-controller.sh $2
+
+    exit 1;
+}
 # print help messages
 CONTROLLER_FOLDER="controller";
 REPOSITORY_FOLDER="repository";
@@ -99,7 +136,18 @@ case "$commands" in
  "entity") shift 1; create_entity "$@" ;;
  #case 5
  "e") shift 1 ;create_entity "$@" ;;
- 
+ #case 6
+ "repository") shift 1; create_repository "$@";;
+ #case 7
+ "r" ) shift 1; create_repository "$@" ;;
+ #case 8
+ "controller") create_controller $2 $3;;
+ #case 9
+ "c") create_controller $2 $3;;
+ #case 10
+ "service") echo "service";;
+ #case 11
+ "s") echo "ss";;
  esac
 
 #./script/create-controller.sh movie
--- java-spring-script/model/User.java
+++ java-spring-script/model/User.java
@@ -19,3 +19,45 @@ public class User implements Serializable {
       this.name = name;
   }
 }
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "movie", schema = "public")
+public class User implements Serializable {
+
+  @Column(name = "name")
+  @NotBlank
+  private String name;
+  public String getName() {
+      return this.name;
+  }
+  public void setName(String name){
+      this.name = name;
+  }
+}
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "movie", schema = "public")
+public class User implements Serializable {
+
+  @Column(name = "name")
+  @NotBlank
+  private String name;
+  public String getName() {
+      return this.name;
+  }
+  public void setName(String name){
+      this.name = name;
+  }
+}
